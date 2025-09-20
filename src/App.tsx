585298a8d40327cad90d9a7dc8218fc35e8f9fea
Pasynkov Evgeny <zhenjapasynkov972@gmail.com>
2025-09-20 13:05:44 +0300

    starting counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [value, setValue] = useState(0)
+
+  useEffect(() => {
+    let getVal = localStorage.getItem("counterValue")
+    if(getVal) {
+      let parseVal = JSON.parse(getVal)
+      setValue(parseVal)
+    }
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem("counterValue", JSON.stringify(value))
+  }, [value])
+
+  const incHandler = () => {
+    setValue(value + 1)
+  }
+
+  // const setLocalStorageHandler = () => {
+  //   localStorage.setItem("counterValue", JSON.stringify(value))
+  //   localStorage.setItem("counterValue2", JSON.stringify(value + 10))
+  // }
+
+  // const getLocalStorageHandler = () => {
+  //   let parseValue = localStorage.getItem("counterValue")
+  //   if (parseValue) {
+  //     setValue(JSON.parse(parseValue))
+  //   }
+  // }
+
+  const clearStorageHandler = () => {
+    localStorage.clear()
+    setValue(0)
+  }
+
+  // const remItemHandler = () => {
+  //   localStorage.removeItem("counterValue2")
+  // }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{value}</h1>
+      <button onClick={incHandler}>inc</button>
+      {/* <button onClick={setLocalStorageHandler}>setLocalStorage</button>
+      <button onClick={getLocalStorageHandler}>getLocalStorage</button> */}
+      <button onClick={clearStorageHandler}>clearStorage</button>
+      {/* <button onClick={remItemHandler}>remItem</button> */}
     </div>
   );
 }
